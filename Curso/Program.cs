c68137c8c44ef979cea6c3405b4111bb84325de3
farleybsd <farley.t.i@hotmail.com>
2021-05-08 01:09:36 -0300

    Criado Projeto

---
+++ Curso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curso.Ef.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
