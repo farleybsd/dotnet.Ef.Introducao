f947e09419c0847d1e46c1b93f7b24b4c41b043c
farleybsd <farley.t.i@hotmail.com>
2021-05-09 16:37:12 -0300

    Criação da classe de contexto

---
+++ Curso/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.Ef.Core.Data
+{
+    public class ApplicationContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CursoEfCore;Integrated Security=True;");
+        }
+    }
+}
