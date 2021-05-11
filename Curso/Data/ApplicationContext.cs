7d24c29f299f0cea3a277ed2c10284bc5ee3a379
farleybsd <farley.t.i@hotmail.com>
2021-05-11 00:27:57 -0300

    Inserindo registros

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -10,6 +10,8 @@ namespace Curso.Ef.Core.Data
     public class ApplicationContext :DbContext
     {
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CursoEfCore;Integrated Security=True;");
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Curso.Ef.Core.Data;
+using Curso.Ef.Core.Domain;
+using Curso.Ef.Core.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
@@ -10,10 +12,34 @@ namespace Curso.Ef.Core
         static void Main(string[] args)
         {
             //Execultando Migrate em dev
-            using var db = new ApplicationContext();
+            //using var db = new ApplicationContext();
             /// db.Database.Migrate();
-            var existerMigracaoPedente = db.Database.GetPendingMigrations().Any();
-            
+            //var existerMigracaoPedente = db.Database.GetPendingMigrations().Any();
+
+            InserirDados();
+            Console.ReadKey();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras="123456987",
+                Valor=10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo=true
+            };
+
+            using var db = new ApplicationContext();
+            //db.Produtos.Add(produto);
+            //db.Set<Produto>().Add(produto);
+            //db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+
+            Console.WriteLine($"Total Registro(s) {registros}");
         }
     }
 }
