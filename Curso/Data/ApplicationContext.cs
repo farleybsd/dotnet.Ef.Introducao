1710e4914d6d00063244647b4c9ac36a2b558f54
farleybsd <farley.t.i@hotmail.com>
2021-05-11 21:59:16 -0300

    Alterando o nome da tabela de histórico de migrações

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -24,7 +24,7 @@ namespace Curso.Ef.Core.Data
                 p=> p.EnableRetryOnFailure(
                     maxRetryCount: 2,
                     maxRetryDelay: TimeSpan.FromSeconds(5),
-                    errorNumbersToAdd:null));
+                    errorNumbersToAdd:null).MigrationsHistoryTable("curso_ef_core"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
